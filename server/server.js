b72bdd407854307de6635ec972cf96d8d0675d99
MACBOOK SOFTUS <developer@softusco.com>
2020-06-15 21:08:56 -0500

    se habilita carpeta estatica para consultar las imagenes

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const { readFileSync } = require("fs");
 const { join } = require("path");
 const resolvers = require("./lib/resolvers");
 const cors = require("cors");
+const path = require('path');
 
 const typeDefs = readFileSync(
   join(__dirname, "lib", "schema.graphql"),
@@ -16,6 +17,7 @@ const schema = makeExecutableSchema({ typeDefs, resolvers });
 var app = express();
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'resources/images')));
 app.use(
   "/api",
   graphqlHTTP({
